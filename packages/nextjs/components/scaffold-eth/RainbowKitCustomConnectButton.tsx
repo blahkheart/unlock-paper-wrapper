69c64693142a21b3706fee0414d310c3d806e983
DannyThomx <dannithomx@gmail.com>
2023-09-24 13:20:57 +0100

    fixed SelectNetwork dropdown not closing when network is selected, refactored SelectNetwork.tsx and RainbowKitCustomConnectButton.tsx

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
@@ -1,22 +1,15 @@
 import { SelectNetwork } from "./SelectNetwork";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useDisconnect, useSwitchNetwork } from "wagmi";
-import { ArrowLeftOnRectangleIcon, ArrowsRightLeftIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
-import { useScaffoldConfig } from "~~/context/ScaffoldConfigContext";
 import { useNetworkColor } from "~~/hooks/scaffold-eth";
-import { enabledChains } from "~~/services/web3/wagmiConnectors";
-
-// import { getTargetNetwork } from "~~/utils/scaffold-eth";
 
 /**
  * Custom Wagmi Connect Button (watch balance + custom design)
  */
 export const RainbowKitCustomConnectButton = () => {
   const networkColor = useNetworkColor();
-  const { disconnect } = useDisconnect();
-  const { switchNetwork } = useSwitchNetwork();
-  const { configuredNetwork, setNetwork } = useScaffoldConfig();
+
   return (
     <ConnectButton.Custom>
       {({ account, chain, openAccountModal, openConnectModal, mounted }) => {
@@ -33,44 +26,6 @@ export const RainbowKitCustomConnectButton = () => {
                 );
               }
 
-              if (chain.unsupported || chain.id !== configuredNetwork.id) {
-                return (
-                  <div className="dropdown dropdown-end">
-                    <label
-                      tabIndex={0}
-                      className={`btn btn-sm dropdown-toggle ${chain.unsupported ? "btn-error" : "btn-warning"}`}
-                    >
-                      <span>{chain.unsupported ? "Wrong network" : "Switch network"}</span>
-                      <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
-                    </label>
-                    <ul tabIndex={0} className="dropdown-content menu p-2 mt-1 shadow-lg bg-base-100 rounded-box">
-                      {enabledChains.map(chain => (
-                        <li key={chain.id}>
-                          <button
-                            className="menu-item"
-                            type="button"
-                            onClick={() => {
-                              setNetwork(chain);
-                              switchNetwork?.(chain.id);
-                            }}
-                          >
-                            <ArrowsRightLeftIcon className="h-6 w-4 ml-2 sm:ml-0" />
-                            <span className="whitespace-nowrap">
-                              Switch to <span style={{ color: networkColor }}>{chain.name}</span>
-                            </span>
-                          </button>
-                        </li>
-                      ))}
-                      <li>
-                        <button className="menu-item text-error" type="button" onClick={() => disconnect()}>
-                          <ArrowLeftOnRectangleIcon className="h-6 w-4 ml-2 sm:ml-0" /> <span>Disconnect</span>
-                        </button>
-                      </li>
-                    </ul>
-                  </div>
-                );
-              }
-
               return (
                 <div className="px-2 flex justify-end items-center">
                   <div className="flex justify-center items-center border-1 rounded-lg">
@@ -78,19 +33,21 @@ export const RainbowKitCustomConnectButton = () => {
                       <span className="text-xs mr-2" style={{ color: networkColor }}>
                         {account.displayBalance}
                       </span>
-                      <SelectNetwork network={chain.name} />
+                      <SelectNetwork chain={chain} />
                     </div>
-                    <button
-                      onClick={openAccountModal}
-                      type="button"
-                      className="btn btn-secondary btn-sm pl-0 pr-2 shadow-md"
-                    >
-                      <BlockieAvatar address={account.address} size={24} ensImage={account.ensAvatar} />
-                      <span className="ml-2 mr-1">{account.displayName}</span>
-                      <span>
-                        <ChevronDownIcon className="h-6 w-4" />
-                      </span>
-                    </button>
+                    {!chain.unsupported && (
+                      <button
+                        onClick={openAccountModal}
+                        type="button"
+                        className="btn btn-secondary btn-sm pl-0 pr-2 shadow-md"
+                      >
+                        <BlockieAvatar address={account.address} size={24} ensImage={account.ensAvatar} />
+                        <span className="ml-2 mr-1">{account.displayName}</span>
+                        <span>
+                          <ChevronDownIcon className="h-6 w-4" />
+                        </span>
+                      </button>
+                    )}
                   </div>
                 </div>
               );
--- packages/nextjs/components/scaffold-eth/SelectNetwork.tsx
+++ packages/nextjs/components/scaffold-eth/SelectNetwork.tsx
@@ -1,56 +1,87 @@
+import { useEffect, useRef, useState } from "react";
 import { useDisconnect, useSwitchNetwork } from "wagmi";
 import { ArrowLeftOnRectangleIcon, ArrowsRightLeftIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
 import { useScaffoldConfig } from "~~/context/ScaffoldConfigContext";
 import { useNetworkColor } from "~~/hooks/scaffold-eth";
 import { enabledChains } from "~~/services/web3/wagmiConnectors";
 
-// import { getTargetNetwork } from "~~/utils/scaffold-eth";
+type IChain = {
+  hasIcon: boolean;
+  iconUrl?: string;
+  iconBackground?: string;
+  id: number;
+  name?: string;
+  unsupported?: boolean;
+};
 
 interface ISelectNetwork {
-  network: string | undefined;
+  chain: IChain;
 }
 
 /**
  * Custom Wagmi Connect Button (watch balance + custom design)
  */
-export const SelectNetwork: React.FC<ISelectNetwork> = ({ network }) => {
+export const SelectNetwork: React.FC<ISelectNetwork> = ({ chain }) => {
   const networkColor = useNetworkColor();
   const { disconnect } = useDisconnect();
   const { switchNetwork } = useSwitchNetwork();
-  //   const configuredNetwork = getTargetNetwork();
-  const { setNetwork } = useScaffoldConfig();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const { configuredNetwork, setNetwork } = useScaffoldConfig();
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (dropdownRef.current && event.target instanceof Node && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
-      <div className="dropdown dropdown-end mr-2">
-        <label tabIndex={0} className="btn  btn-sm dropdown-toggle">
-          <span>{network}</span>
+      <div className="dropdown dropdown-end mr-2" ref={dropdownRef}>
+        <label
+          tabIndex={0}
+          className={`btn btn-sm dropdown-toggle ${chain.unsupported ? "btn-error" : "btn-warning"}`}
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        >
+          <span className="select-network-text">
+            {chain.unsupported ? "Wrong network" : chain.id !== configuredNetwork.id ? "Switch network" : chain.name}
+          </span>
           <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
         </label>
-        <ul tabIndex={0} className="dropdown-content menu p-2 mt-1 shadow-lg bg-base-100 rounded-box">
-          {enabledChains.map(chain => (
-            <li key={chain.id}>
-              <button
-                className="menu-item"
-                type="button"
-                onClick={() => {
-                  switchNetwork?.(chain.id);
-                  setNetwork(chain);
-                }}
-              >
-                <ArrowsRightLeftIcon className="h-6 w-4 ml-2 sm:ml-0" />
-                <span className="whitespace-nowrap">
-                  Switch to <span style={{ color: networkColor }}>{chain.name}</span>
-                </span>
+        {isDropdownOpen && (
+          <ul tabIndex={0} className="dropdown-content menu p-2 mt-1 shadow-lg bg-base-100 rounded-box">
+            {enabledChains.map(chain => (
+              <li key={chain.id}>
+                <button
+                  className="menu-item"
+                  type="button"
+                  onClick={() => {
+                    switchNetwork?.(chain.id);
+                    setNetwork(chain);
+                    setIsDropdownOpen(false);
+                  }}
+                >
+                  <ArrowsRightLeftIcon className="h-6 w-4 ml-2 sm:ml-0" />
+                  <span className="whitespace-nowrap">
+                    Switch to <span style={{ color: networkColor }}>{chain.name}</span>
+                  </span>
+                </button>
+              </li>
+            ))}
+            <li>
+              <button className="menu-item text-error" type="button" onClick={() => disconnect()}>
+                <ArrowLeftOnRectangleIcon className="h-6 w-4 ml-2 sm:ml-0" /> <span>Disconnect</span>
               </button>
             </li>
-          ))}
-          <li>
-            <button className="menu-item text-error" type="button" onClick={() => disconnect()}>
-              <ArrowLeftOnRectangleIcon className="h-6 w-4 ml-2 sm:ml-0" /> <span>Disconnect</span>
-            </button>
-          </li>
-        </ul>
+          </ul>
+        )}
       </div>
     </>
   );
